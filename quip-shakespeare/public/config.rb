101f1748f8ef738454719f4f280f4ecc917e067e
Kay Krewson <kay.k@comcast.net>
2017-07-14 21:10:25 -0700

    initial bare bones setup for main html stuff sass css etc

--- quip-shakespeare/public/config.rb
+++ quip-shakespeare/public/config.rb
@@ -1,13 +1,11 @@
-here’s what goes in the config.rb:
-
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "scss"
-images_dir = "../images"
-javascripts_dir = "."
+images_dir = "images"
+javascripts_dir = "scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
