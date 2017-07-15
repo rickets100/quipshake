b59d0546528a133cf6b699297ceb00d7c827937a
Kay Krewson <kay.k@comcast.net>
2017-07-15 14:10:13 -0700

    add a file that is just note-taking for a possible added feature

---
+++ quip-shakespeare/bin/data-sources/links.js
@@ -0,0 +1,5 @@
+
+/*
+https://labs.jstor.org/shakespeare/
+Understanding Shakespeare is a collaborative project between JSTOR Labs and the Folger Shakespeare Library . It’s a research tool that allows students, educators and scholars to use the text of Shakespeare’s plays to quickly navigate into the scholarship written about them—line by line. Users simply click next to any line of text in a play and relevant articles from the JSTOR archive immediately load.
+*/
