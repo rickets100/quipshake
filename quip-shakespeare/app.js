5aeeea4230c97936c95a0cdff621ab64be9784c6
Kay Krewson <kay.k@comcast.net>
2017-07-17 10:39:13 -0700

    changing app.module to point to heroku database

--- quip-shakespeare/app.js
+++ quip-shakespeare/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 var apiRoutes = require('./routes/api') // api.js in routes
 // var users = require('./routes/users')
--- quip-shakespeare/public/scripts/app.module.js
+++ quip-shakespeare/public/scripts/app.module.js
@@ -3,6 +3,6 @@
   console.log('APP.MODULE.JS')
   'use strict'
   angular.module('app', ['ui.router'])
-  // .constant('API_BASE_URL', 'https://yournamehere.herokuapp.com')
-  .constant('API_BASE_URL', 'http://localhost:3009')
+  .constant('API_BASE_URL', 'https://quipshake.herokuapp.com')
+  // .constant('API_BASE_URL', 'http://localhost:3009')
 })()
